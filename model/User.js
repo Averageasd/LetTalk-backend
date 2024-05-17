5efc5c31adc090533c85aeff50c11507566df5c3
Nguyen Huynh <nhuynh9497@gmail.com>
2024-05-17 00:37:31 -0400

    finished backend logic for sending/accepting requests

--- model/User.js
+++ model/User.js
@@ -5,8 +5,9 @@ const UserSchema = new Schema(
     {
         name: {type: String, require: true, minLength: 3, maxLength: 20},
         password: {type: String, require: true, minLength: 8},
-        rooms: [{type:Schema.Types.ObjectId, ref:'room'}],
-        invitations: [{type:Schema.Types.ObjectId, ref:'invitation'}]
+        rooms: [{type: Schema.Types.ObjectId, ref: 'room'}],
+        invitations: [{type: Schema.Types.ObjectId, ref: 'invitation'}],
+        connections: [{type: Schema.Types.ObjectId, ref: 'user'}]
     }
 );
 
--- socket/IOSingleton.js
+++ socket/IOSingleton.js
@@ -54,6 +54,7 @@ class IOSingleton {
                     messages: [],
                 })
 
+                console.log(newRoom);
                 await newRoom.save();
                 const newInvitation = new Invitation({
                     status: 'PENDING',
@@ -62,15 +63,15 @@ class IOSingleton {
                     room: newRoom._id,
                 })
                 await newInvitation.save();
+                IOSingleton.IOInstance.emit('send-connect-request', from, to);
             });
 
-            socket.on('connection-request-response', async (userId, accept, invitationId) => {
+            socket.on('connection-request-response', async (userId, toUserId, accept, invitationId) => {
                 console.log('yo');
                 if (accept) {
                     const invitation = await Invitation.findByIdAndUpdate(invitationId, {
                         'status': 'ACCEPTED'
                     }, {new: true}).exec();
-
                     const room = await RoomModel.findById(invitation.room).exec();
                     const updatedRoomWithNewUsers = [...room.users];
                     updatedRoomWithNewUsers.push(invitation.from);
@@ -78,9 +79,20 @@ class IOSingleton {
                     await RoomModel.findByIdAndUpdate(room._id, {
                         'users': updatedRoomWithNewUsers
                     }).exec();
-                }
-            });
+                    const sendingRequestUser = await UserModel.findById(toUserId).exec();
+                    const requestReceiver = await UserModel.findById(userId).exec();
+                    await UserModel.findByIdAndUpdate(userId, {
+                        rooms: requestReceiver.rooms.concat(room._id),
+                        connections: requestReceiver.connections.concat(toUserId)
+                    });
 
+                    await UserModel.findByIdAndUpdate(toUserId, {
+                        rooms: sendingRequestUser.rooms.concat(room._id),
+                        connections: sendingRequestUser.connections.concat(userId)
+                    });
+                }
+                IOSingleton.IOInstance.emit('connection-request-response', userId, toUserId, accept, invitationId);
+            });
 
         });
         return IOSingleton.IOInstance;
